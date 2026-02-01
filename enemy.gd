47e7ca8894db34f6fed08f70bd111681508de1d2
Federico Sbressa <sbreezerick@gmail.com>
2026-02-01 02:47:34 +0100

    Fixed enemy movement and animation
    It's 3.00 AM I should go to bed

--- enemy.gd
+++ enemy.gd
@@ -1,34 +1,65 @@
 extends CharacterBody2D
 
+
+
 @export var ghost_path: PathFollow2D
+
 @onready var anim_player = $AnimatedSprite2D
 
+
+
 func _physics_process(_delta):
+
 	if not ghost_path: return
-	
-	# Seguiamo il fantasma
+
+
+# Seguiamo il fantasma
+
 	velocity = ghost_path.real_direction * 150.0 # Usa la velocità dell'NPC
+
 	move_and_slide()
-	
+
+
 	_update_animation(ghost_path.real_direction)
 
+
+
 func _update_animation(dir: Vector2):
+
 	if dir == Vector2.ZERO:
-		anim_player.play("idle")
+
+		anim_player.play("front_idle")
+
 		return
 
+
+
 	# Determina la direzione prevalente
+
 	if abs(dir.x) > abs(dir.y):
+
 		if dir.x > 0:
+
 			anim_player.flip_h = false
-			anim_player.play("front_walk")
+
+			anim_player.play("side_walk")
+
 		else:
+
 			anim_player.flip_h = true
-			anim_player.play("walk_left")
+
+			anim_player.play("side_walk")
+
 	else:
+
 		if dir.y > 0:
+
 			anim_player.flip_h = false
-			anim_player.play("walk_down")
+
+			anim_player.play("front_walk")
+
 		else:
+
 			anim_player.flip_h = false
-			anim_player.play("walk_up")
+
+			anim_player.play("back_walk")
--- path_follow_2d.gd
+++ path_follow_2d.gd
@@ -1,22 +1,43 @@
 extends PathFollow2D
 
+
+
 @export var patrol_speed: float = 120.0
+
 var direction: int = 1
+
 var last_position: Vector2
+
 var real_direction: Vector2 = Vector2.ZERO
 
+
+
 func _ready():
+
 	last_position = global_position
 
+
+
 func _process(delta):
-	last_position = global_position # Salva la posizione prima del movimento
-	
+
+
+
 	progress += patrol_speed * delta * direction
-	
-	# Calcola il vettore direzione reale
-	if global_position != last_position:
+
+
+# Calcola il vettore direzione reale
+
+	var current_pos = global_position
+	if current_pos.distance_to(last_position) > 0.1:
+
 		real_direction = (global_position - last_position).normalized()
-	
-	# Ping-Pong logic
-	if progress_ratio >= 1.0: direction = -1
-	elif progress_ratio <= 0.0: direction = 1
+
+
+	last_position = global_position # Salva la posizione prima del movimento
+# Ping-Pong logic
+
+	if progress_ratio >= 1.0: 
+		direction = -1
+
+	elif progress_ratio <= 0.0: 
+		direction = 1
