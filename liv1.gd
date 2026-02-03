746b0c054aafae148dcb2adfb30b970fef11c527
Federico Sbressa <sbreezerick@gmail.com>
2026-02-03 21:58:33 +0100

    Add call_deferred() function at every change of scene
    To avoid unexpected exceptions

---
+++ liv1.gd
@@ -0,0 +1,11 @@
+extends Node2D
+@export var next_scene_path: String = "res://liv2.tscn"
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+		$CravattaMeccanico.visible = false
+		 
+
+
+func _on_next_level_body_entered(body):
+	get_tree().call_deferred("change_scene_to_file", next_scene_path)
+	
+	
